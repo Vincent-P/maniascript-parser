0946fda4db85079779f522347a1e60b7258dfd51
Vincent Parizet <vincent.parizet@hotmail.fr>
2020-01-29 21:25:46 +0100

    Clean 🗑️

--- lib_maniascript/src/document/type.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Debug)]
-pub enum Type {
-    Simple(String),
-    Array(String, Option<Box<Type>>),
-    Function(Box<Type>, Vec<Type>),
-    Dummy,
-}
--- lib_maniascript/src/lib.rs
+++ lib_maniascript/src/lib.rs
@@ -1,6 +1,1 @@
-pub mod ast;
-pub mod document;
-pub mod formatter;
-pub mod lexer;
 pub mod parser;
-pub mod validator;
